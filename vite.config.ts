174dea948e8995e582ed5a4670b8dfc3374a10ca
ryeswanthkumar <yeswanthkumar348@gmail.com>
2026-08-13 18:26:39 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PyCRM_UIgit add package.json vite.config.ts/',
+  base: '/PyCRM_UI/',
 })
